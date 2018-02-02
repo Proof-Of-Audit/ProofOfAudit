0698eefa693b5828b848365a8146c682667a9cfb
Zaki Manian <zaki@manian.org>
2018-02-02 14:52:49 +0000

    Fix site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://proofofaudit.com'
+SITEURL = 'http://proof-of-audit.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
